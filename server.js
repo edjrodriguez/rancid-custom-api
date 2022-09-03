ccbe1fb15fdd7a679199fa3f23e327c4b3150e1e
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-09-03 10:09:04 -0600

    Refactor data and get functions

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const movieData = require("./movieData.json")  
 const app = express();
+const cors = require('cors')
+
+app.use(cors());
 
 app.set('port', process.env.PORT || 3002);
 app.locals.title = 'Genre API';
@@ -13,9 +16,22 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
 
-app.locals.movies = [movieData]
+app.locals.movies = movieData
 
 app.get('/movies', (request, response) => {
     const movies = app.locals.movies
-    response.send(movies);
+    response.status(200).json(movies);
+  });
+
+
+  app.get('/movies/:id', (request, response) => {
+    console.log(app.locals.movies)
+    const { id } = request.params
+    const movie = app.locals.movies.movies.find(movie => movie.id == id)
+    if(!movie) {
+        return response.sendStatus(404)
+    }
+    const data = {movie: movie}
+    response.status(200).json(data);
+   
   });
