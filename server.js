5e8ae45e0b694cbdb693ae846d1ac8808c78b38c
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-09-09 16:51:28 -0600

    Add Profcfile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 app.use(cors());
 
-app.set('port', process.env.PORT || 3002);
+app.set('port',  3002);
 app.locals.title = 'Genre API';
 
 app.get('/', (request, response) => {
